332080cfc71355d3e55ef84af59d6d3636a03515
rpatel021 <135402730+rpatel021@users.noreply.github.com>
2026-09-20 09:22:23 -0500

    Restore app.js party picker

--- app.js
+++ app.js
@@ -168,12 +168,10 @@ function render() {
   `;
 }
 
-Promise.all([
-  fetch("./plans.json").then((r) => r.json()),
-  fetch("./plans-weekend.json").then((r) => r.json()).catch(() => []),
-])
-  .then(([a, b]) => {
-    plans = [...a, ...b].sort((a, b) => String(a.for_night).localeCompare(String(b.for_night)));
+fetch("./plans.json")
+  .then((r) => r.json())
+  .then((data) => {
+    plans = [...data].sort((a, b) => String(a.for_night).localeCompare(String(b.for_night)));
     buildTrack();
     wireParty();
     const t = tonightIso();
